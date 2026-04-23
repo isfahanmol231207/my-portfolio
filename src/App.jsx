72f690d3df8554542855d6ebc001b39db9fc5de9
isfahanmol231207 <153908817+isfahanmol231207@users.noreply.github.com>
2026-04-23 15:48:27 +0500

    Fix component file names

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import Skills from "./components/skills";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 
