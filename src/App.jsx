62fc68c16be707803a4e835fd536f19eaf9149ed
isfahanmol231207 <153908817+isfahanmol231207@users.noreply.github.com>
2026-04-23 16:00:28 +0500

    Fix case sensitivity errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/skills";
-import Experience from "./components/Experience";
+import Experience from "./components/experience";
 import Contact from "./components/Contact";
 
 function App() {
